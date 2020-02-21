8b2285191533f288ae224360e00abaa9431aba4c
Nick Solonyy <nicksolony@gmail.com>
2020-02-21 04:13:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+
+end
+=======
+  belongs_to :network
+
+  def actors_list
+    self.actors.collect {|actor| actor.full_name}
+  end
 end
